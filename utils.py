163233277bbbff06ae672fd29db0537ad66a5ff1
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2024-06-23 17:23:42 +0300

    Modified console output

--- utils.py
+++ utils.py
@@ -3,5 +3,11 @@ def log_book_data(title, author):
     return
 
 
-def print_book_data(title, author):
-    print(f"Title: {title}, Author: {author}")
+# Define the column widths
+filename_width = 60
+title_width = 40
+author_width = 20
+
+
+def print_book_data(filename, title, author):
+    print(f"{filename:<{filename_width}} {title:<{title_width}} {author:<{author_width}}")
